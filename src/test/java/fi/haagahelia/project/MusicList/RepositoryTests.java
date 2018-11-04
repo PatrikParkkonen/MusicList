21dc63e667d01387e368e3b3097a65af1bd7257f
SephKoulu <patrik.parkkonen@gmail.com>
2018-11-04 13:52:48 +0200

    Styles etc

---
+++ src/test/java/fi/haagahelia/project/MusicList/RepositoryTests.java
@@ -0,0 +1,58 @@
+package fi.haagahelia.project.MusicList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import fi.haagahelia.project.MusicList.domain.Song;
+import fi.haagahelia.project.MusicList.domain.Genre;
+import fi.haagahelia.project.MusicList.domain.User;
+import fi.haagahelia.project.MusicList.domain.SongRepository;
+import fi.haagahelia.project.MusicList.domain.GenreRepository;
+import fi.haagahelia.project.MusicList.domain.UserRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class RepositoryTests {
+
+    @Autowired
+    private SongRepository srepository;
+    
+    @Autowired
+    private GenreRepository grepository;
+    
+    @Autowired
+    private UserRepository urepository;
+
+   
+    
+    @Test
+    public void createNewSong() {
+    	Song song = new Song("Valtiaan Uudet Vaateet", "Stam1na", "Nocebo", "4:34", 2012, new Genre("Metal"));
+    	srepository.save(song);
+    	assertThat(song.getId()).isNotNull();
+    }
+    
+    @Test
+    public void createNewGenre() {
+    	Genre genre = new Genre("Testi");
+    	grepository.save(genre);
+    	assertThat(genre.getGenreid()).isNotNull();
+    }
+    
+   @Test
+   public void createNewUser() {
+	   User user = new User("guest", "guest", "GUEST");
+	   urepository.save(user);
+	   assertThat(user.getUsername()).isNotNull();
+   }
+
+}
